967029bc6916583cd83973e7ded7e40020fca2dd
Peter Izsak <peter.izsak@intel.com>
2018-05-08 16:42:13 +0300

    Minor

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -142,12 +142,12 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_theme_options = {
     'canonical_url': '',
-    'analytics_id': '',
+    # 'analytics_id': '',
     'logo_only': True,
     'display_version': False,
     'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
+    # 'style_external_links': False,
+    # 'vcs_pageview_mode': '',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
