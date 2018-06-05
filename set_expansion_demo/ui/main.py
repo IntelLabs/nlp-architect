4183cfe03c0c1339f4f904be3bf7f393f759a5c9
Shira Guskin <shira.guskin@intel.com>
2018-06-05 12:18:08 +0300

    sync table and vocab selections

--- set_expansion_demo/ui/main.py
+++ set_expansion_demo/ui/main.py
@@ -6,6 +6,7 @@ from bokeh.models.widgets.tables import BooleanFormatter, CheckboxEditor
 from bokeh.core.enums import Enumeration, enumeration
 from bokeh.core.properties import Enum
 from bokeh.io import curdoc
+from bokeh.models.selections import Selection
 # from nlp_architect.utils.text_preprocess import simple_normalizer
 import numpy as np
 import pandas
@@ -21,9 +22,9 @@ out_path = "export.csv"
 hash2group = {}
 all_phrases = []
 max_visible_phrases = 5000
-seed_after_search = ''
 working_text = 'working...'
-
+all_selected_phrases = []
+search_flag = False
 
 def get_phrases(top_n=100000):
     global all_phrases
@@ -93,12 +94,63 @@ def conv(val):
 
 
 def row_selected_callback(attr, old, new):
-    selected_rows = new.indices
-    values = ''
-    for x in selected_rows:
-        values += (expand_table_source.data['res'][x] + ', ')
-    values = values[:-2]
-    seed_input_box.value = values
+    print('row selected callback')
+    print('old indices=' + str(old.indices))
+    print('new indices=' + str(new.indices))
+    global all_selected_phrases
+    # selected_rows = new.indices
+    # values = ''
+    # for x in selected_rows:
+    #     values += (expand_table_source.data['res'][x] + ', ')
+    # values = values[:-2]
+
+
+    #sync phrases lists:
+
+    old_phrases = [expand_table_source.data['res'][p] for p in old.indices]
+    new_phrases = [expand_table_source.data['res'][p] for p in new.indices]
+    print('selected_expand was updated: old=' + str(
+        old_phrases) + ' ,new=' + str(new_phrases))
+    #phrase was de-selected from expand list:
+    for o in old_phrases:
+        if o not in new_phrases and o in phrases_list.value:
+            print('removing ' + o + 'from vocab selected')
+            phrases_list.value.remove(o)
+            break
+    #new phrase was selected from expand list:
+    for n in new_phrases:
+        if n not in old_phrases and n in phrases_list.options and n not in phrases_list.value:
+            phrases_list.value.append(n)
+            break
+
+    update_all_selected_phrases()
+    seed_input_box.value = get_selected_phrases()
+
+
+def update_all_selected_phrases():
+    print('update selected phrases')
+    global all_selected_phrases
+    updated_selected_phrases = all_selected_phrases[:]
+    selected_expand = [expand_table_source.data['res'][p] for p in expand_table_source.selected.indices]
+    selected_vocab = phrases_list.value
+    print('selected expand=' + str(selected_expand))
+    print('selected vocab=' + str(selected_vocab))
+    print('current all_selected_phrases=' + str(all_selected_phrases))
+    for x in all_selected_phrases:
+        print('x=' + x)
+        if (x in expand_table_source.data['res'] and x not in selected_expand) or (x in phrases_list.options and x not in selected_vocab):
+            print('removing ' + x)
+            updated_selected_phrases.remove(x)
+    for e in selected_expand:
+        if e not in updated_selected_phrases:
+            print('adding ' + e)
+            updated_selected_phrases.append(e)
+    for v in selected_vocab:
+        if v not in updated_selected_phrases:
+            print('adding ' + v)
+            updated_selected_phrases.append(v)
+    all_selected_phrases = updated_selected_phrases[:]
+    print('all_selected_phrases list was updated: ' + str(all_selected_phrases))
 
 
 def show_phrases_callback(checked_value):
@@ -141,22 +193,57 @@ def get_expand_results_callback():
 
 
 def search_callback(value, old, new):
-    global all_phrases, phrases_list, seed_after_search
+    print('search vocab')
+    global all_phrases, phrases_list, all_selected_phrases, search_flag
+    search_flag = True
     # new_phrases = [x for x in all_phrases if x.startswith(new)]
     new_phrases = [x for x in all_phrases if (new.lower() in x.lower() or x.lower()==new.lower())]
     phrases_list.options=new_phrases[0:max_visible_phrases]
-    seed_after_search = seed_input_box.value
+    phrases_list.value = [x for x in all_selected_phrases if x in phrases_list.options]
+    print('selected vocab after search=' + str(phrases_list.value))
+    search_flag = False
 
 
-def phrase_selected_callback(attr, old, selected_phrases):
-    values = '' if len(search_input_box.value) == 0 else seed_after_search + ', '
-    for x in selected_phrases:
-        values += x + ', '
-    values = values[:-2]
-    seed_input_box.value = values
+def vocab_phrase_selected_callback(attr, old_selected, new_selected):
+    print('vocab selected')
+    global all_selected_phrases, search_flag
+    if(search_flag):
+        return
+    # values = ''
+    # for x in selected_phrases:
+    #     values += x + ', '
+    # values = values[:-2]
+
+
+    print('selected_vocab was updated: old=' + str(old_selected) + ' ,new=' + str(new_selected))
+    # sync expand table:
+
+    # phrase was de-selected from vocab list:
+    expand_selected = [expand_table_source.data['res'][p] for p in expand_table_source.selected.indices]
+    for o in old_selected:
+        if o not in new_selected and o in expand_selected:
+            print(o + ' removed from vocab selected and exists in expand selected')
+            print('removing ' + o + 'from expand selected indices. index=' + str(expand_table_source.data['res'].index(o)))
+            print('current expand indices: ' + str(expand_table_source.selected.indices))
+            expand_table_source.selected.indices.remove(expand_table_source.data['res'].index(o))
+            print('new expand indices: ' + str(expand_table_source.selected.indices))
+
+            break
+    # new phrase was selected from vocab list:
+    for n in new_selected:
+        if n not in old_selected and n in expand_table_source.data['res'] and n not in expand_selected:
+            expand_table_source.selected.indices.append(expand_table_source.data['res'].index(n))
+            break
+    update_all_selected_phrases()
+    seed_input_box.value = get_selected_phrases()
 
 
 def clear_seed_callback():
+    print('clear')
+    global all_selected_phrases
+    expand_table_source.selected.indices=[]
+    phrases_list.value = []
+    all_selected_phrases = []
     seed_input_box.value = ''
 
 
@@ -173,12 +260,21 @@ def export_data_callback():
         working_label.text = ''
 
 
+def get_selected_phrases():
+    global all_selected_phrases
+    phrases = ''
+    for x in all_selected_phrases:
+        phrases += x + ', '
+    phrases = phrases[:-2]
+    return phrases
+
+
 # set callbacks
 expand_button.on_click(get_expand_results_callback)
 expand_table_source.on_change('selected', row_selected_callback)
 checkbox_group.on_click(show_phrases_callback)
 search_input_box.on_change('value',search_callback)
-phrases_list.on_change('value', phrase_selected_callback)
+phrases_list.on_change('value', vocab_phrase_selected_callback)
 clear_seed_button.on_click(clear_seed_callback)
 export_button.on_click(export_data_callback)
 
