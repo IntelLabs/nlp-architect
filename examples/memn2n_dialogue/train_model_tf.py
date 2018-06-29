38d889f17edaf0a223b3b3363348c9777ae3c888
andy-nervana <andy@nervanasys.com>
2018-06-28 19:25:06 -0700

    Initial working dialog model in TF

---
+++ examples/memn2n_dialogue/train_model_tf.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+# ******************************************************************************
+# Copyright 2017-2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ******************************************************************************
+"""
+Example that trains an End-to-End Memory Network on the Facebook bAbI
+goal-oriented dialog dataset.
+
+Reference:
+    "Learning End-to-End Goal Oriented Dialog"
+    https://arxiv.org/abs/1605.07683.
+
+Usage:
+
+    python train_memn2n.py --task 5
+
+    use --task to specify which bAbI-dialog task to run on
+        - Task 1: Issuing API Calls
+        - Task 2: Updating API Calls
+        - Task 3: Displaying Options
+        - Task 4: Providing Extra Information
+        - Task 5: Conducting Full Dialogs
+"""
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+import os
+from contextlib import closing
+import numpy as np
+from tqdm import tqdm
+import tensorflow as tf
+from interactive_utils import interactive_loop
+from nlp_architect.data.babi_dialog import BABI_Dialog
+from nlp_architect.models.memn2n_dialogue_tf import MemN2N_Dialog_TF
+from nlp_architect.utils.io import validate_parent_exists, check_size, validate
+
+# parse the command line arguments
+tf.flags.DEFINE_integer(
+    'task',
+    1,
+    'the task ID to train/test on from bAbI-dialog dataset (1-6)')
+tf.flags.DEFINE_integer(
+    'emb_size',
+    32,
+    'Size of the word-embedding used in the model.')
+tf.flags.DEFINE_integer(
+    'batch_size',
+    32,
+    'Size of the batch for optimization.')
+tf.flags.DEFINE_integer(
+    'nhops',
+    3,
+    'Number of memory hops in the network')
+tf.flags.DEFINE_boolean(
+    'use_match_type',
+    False,
+    'use match type features')
+tf.flags.DEFINE_boolean(
+    'cache_match_type',
+    False,
+    'cache match type answers')
+tf.flags.DEFINE_boolean(
+    'cache_vectorized',
+    False,
+    'cache vectorized data')
+tf.flags.DEFINE_boolean(
+    'use_oov',
+    False,
+    'use OOV test set')
+tf.flags.DEFINE_float(
+    'lr',
+    0.001,
+    'learning rate')
+tf.flags.DEFINE_float(
+    'grad_clip_norm',
+    40.0,
+    'Clip gradients such that norm is below this value.')
+tf.flags.DEFINE_float(
+    'eps',
+    1e-8,
+    'epsilon used to avoid divide by zero in softmax renormalization.')
+tf.flags.DEFINE_boolean(
+    'save_log',
+    False,
+    'Save evaluation results to log file.')
+tf.flags.DEFINE_string(
+    'data_dir',
+    './data',
+    'File to save model weights to.')
+tf.flags.DEFINE_string(
+    'log_file',
+    'memn2n_dialgoue_results.txt',
+    'File to write evaluation set results to.')
+tf.flags.DEFINE_string(
+    'weights_save_path',
+    'saved_tf/',
+    'File to save model weights to.')
+tf.flags.DEFINE_integer(
+    'save_epochs',
+    1,
+    'Number of epochs between saving model weights.')
+tf.flags.DEFINE_integer(
+    'epochs',
+    100,
+    'Number of epochs between saving model weights.')
+tf.flags.DEFINE_boolean(
+    'restore',
+    False,
+    'Restore weights if found.')
+tf.flags.DEFINE_boolean(
+    'interactive',
+    False,
+    'enable interactive mode at the end of training.')
+tf.flags.DEFINE_boolean(
+    'test',
+    False,
+    'evaluate on the test set at the end of training.')
+FLAGS = tf.flags.FLAGS
+
+
+validate((FLAGS.emb_size, int, 1, 10000),
+         (FLAGS.eps, float, 1e-15, 1e-2),
+         (FLAGS.lr, float, 1e-8, 10),
+         (FLAGS.grad_clip_norm, float, 1e-3, 1e5))
+
+# Validate inputs
+validate_parent_exists(FLAGS.log_file)
+log_file = FLAGS.log_file
+validate_parent_exists(FLAGS.weights_save_path)
+weights_save_path = FLAGS.weights_save_path
+validate_parent_exists(FLAGS.data_dir)
+data_dir = FLAGS.data_dir
+assert log_file.endswith('.txt')
+
+babi = BABI_Dialog(
+    path=data_dir,
+    task=FLAGS.task,
+    oov=FLAGS.use_oov,
+    use_match_type=FLAGS.use_match_type,
+    cache_match_type=FLAGS.cache_match_type,
+    cache_vectorized=FLAGS.cache_vectorized)
+
+train_set = babi.data_dict['train']
+dev_set = babi.data_dict['dev']
+test_set = babi.data_dict['test']
+
+n_train = train_set['memory']['data'].shape[0]
+n_val = dev_set['memory']['data'].shape[0]
+n_test = test_set['memory']['data'].shape[0]
+
+train_batches = zip(range(0, n_train - FLAGS.batch_size, FLAGS.batch_size),
+                    range(FLAGS.batch_size, n_train, FLAGS.batch_size))
+train_batches = [(start, end) for start, end in train_batches]
+
+val_batches = zip(range(0, n_val - FLAGS.batch_size, FLAGS.batch_size),
+                    range(FLAGS.batch_size, n_val, FLAGS.batch_size))
+val_batches = [(start, end) for start, end in val_batches]
+
+test_batches = zip(range(0, n_test - FLAGS.batch_size, FLAGS.batch_size),
+                    range(FLAGS.batch_size, n_test, FLAGS.batch_size))
+test_batches = [(start, end) for start, end in test_batches]
+
+best_validation_accuracy = 0
+
+with tf.Session() as sess:
+    memn2n = MemN2N_Dialog_TF(
+        babi.cands,
+        FLAGS.batch_size,
+        babi.vocab_size,
+        babi.max_utt_len,
+        babi.memory_size,
+        FLAGS.emb_size,
+        hops=FLAGS.nhops,
+        max_grad_norm=FLAGS.grad_clip_norm,
+        use_match_type=babi.kb_ents_to_cand_idxs,
+        kb_ents_to_type=babi.kb_ents_to_type,
+        kb_ents_to_cand_idxs=babi.kb_ents_to_cand_idxs,
+        match_type_idxs=babi.match_type_idxs,
+        session=sess)
+
+    if FLAGS.restore and os.path.exists(weights_save_path):
+        print("Loading weights from {}".format(weights_save_path))
+        memn2n.saver.restore(sess, weights_save_path)
+    elif FLAGS.restore and os.path.exists(weights_save_path) is False:
+        print("Could not find weights at {}. ".format(weights_save_path)
+              + "Running with random initialization.")
+
+    for e in range(FLAGS.epochs):
+        np.random.shuffle(train_batches)
+        train_cost = []
+
+        for start, end in tqdm(train_batches, total=len(train_batches),
+                               unit='minibatches', desc="Epoch {}".format(e)):
+            s = train_set['memory']['data'][start:end]
+            q = train_set['user_utt']['data'][start:end]
+            a = train_set['answer']['data'][start:end]
+
+            cost = memn2n.batch_fit(s, q, a)
+            train_cost.append(cost)
+
+        train_cost_str = "Epoch {}: train_cost {}".format(e, np.mean(train_cost))
+        print(train_cost_str)
+
+        if FLAGS.save_log:
+            with open(log_file, 'a') as f:
+                f.write(train_cost_str + '\n')
+
+        if e % FLAGS.save_epochs == 0:
+            print("Saving model to {}".format(weights_save_path))
+            memn2n.saver.save(sess, weights_save_path)
+            print("Saving complete")
+
+        val_error = []
+        # Eval after each epoch
+        for start, end in tqdm(val_batches, total=len(val_batches),
+                               unit='minibatches', desc="Epoch {}".format(e)):
+            s = dev_set['memory']['data'][start:end]
+            q = dev_set['user_utt']['data'][start:end]
+            a = dev_set['answer']['data'][start:end]
+
+            a_pred = memn2n.predict(s, q)
+
+            error = np.mean(a.argmax(axis=1) != a_pred)
+            val_error.append(error)
+
+        val_err_str = "Epoch {}: Validation Error: {}".format(e, np.mean(val_error))
+        print(val_err_str)
+        if FLAGS.save_log:
+            with open(log_file, 'a') as f:
+                f.write(val_err_str + '\n')
+
+    print('Training Complete.')
+
+    # need to re-write interactive loop to work with tf model
+    # if FLAGS.interactive:
+    #     interactive_loop(interactive_computation, babi)
+
+    # if FLAGS.test:
+    #     # Final evaluation on test set
+    #     test_loss = []
+    #     test_error = []
+    #     for idx, data in enumerate(test_set):
+    #         test_output = loss_computation(data)
+    #         test_loss.append(np.sum(test_output['test_cross_ent_loss']))
+    #         preds = np.argmax(test_output['test_preds'], axis=1)
+    #         error = np.mean(data['answer'].argmax(axis=1) != preds)
+    #         test_error.append(error)
+
+    #     test_cost_str = "test_cost {}, test_error {}".format(
+    #         np.mean(test_loss), np.mean(test_error))
+    #     print(test_cost_str)
+    #     if FLAGS.save_log:
+    #         with open(log_file, 'a') as f:
+    #             f.write(test_cost_str + '\n')
---
+++ nlp_architect/models/memn2n_dialogue_tf.py
@@ -0,0 +1,222 @@
+"""End-To-End Memory Networks.
+
+The implementation is based on http://arxiv.org/abs/1503.08895 [1]
+"""
+from __future__ import absolute_import
+from __future__ import division
+
+import tensorflow as tf
+import numpy as np
+from six.moves import range
+
+
+def zero_nil_slot(t, name=None):
+    """
+    Overwrites the nil_slot (first row) of the input Tensor with zeros.
+
+    The nil_slot is a dummy slot and should not be trained and influence
+    the training algorithm.
+    """
+    with tf.op_scope([t], name, "zero_nil_slot") as name:
+        t = tf.convert_to_tensor(t, name="t")
+        s = tf.shape(t)[1]
+        z = tf.zeros(tf.stack([1, s]))
+        return tf.concat(axis=0, values=[z, tf.slice(t, [1, 0], [-1, -1])], name=name)
+
+
+class MemN2N_Dialog_TF(object):
+    """End-To-End Memory Network."""
+    def __init__(self,
+        cands,
+        batch_size,
+        vocab_size,
+        sentence_size,
+        memory_size,
+        embedding_size,
+        hops=3,
+        max_grad_norm=40.0,
+        use_match_type=False,
+        kb_ents_to_type=None,
+        kb_ents_to_cand_idxs=None,
+        match_type_idxs=None,
+        nonlin=None,
+        initializer=tf.random_normal_initializer(stddev=0.1),
+        optimizer=tf.train.AdamOptimizer(learning_rate=0.01),
+        session=tf.Session(),
+        name='MemN2N_Dialog'):
+        """Creates an End-To-End Memory Network for Goal Oriented Dialog
+
+        Args:
+            cands: Encoded candidate answers
+
+            batch_size: The size of the batch.
+
+            vocab_size: The size of the vocabulary (should include the nil word). The nil word
+            one-hot encoding should be 0.
+
+            sentence_size: The max size of a sentence in the data. All sentences should be padded
+            to this length. If padding is required it should be done with nil one-hot encoding (0).
+
+            memory_size: The max size of the memory. Since Tensorflow currently does not support jagged arrays
+            all memories must be padded to this length. If padding is required, the extra memories should be
+            empty memories; memories filled with the nil word ([0, 0, 0, ......, 0]).
+
+            embedding_size: The size of the word embedding.
+
+            hops: The number of hops. A hop consists of reading and addressing a memory slot.
+            Defaults to `3`.
+
+            max_grad_norm: Maximum L2 norm clipping value. Defaults to `40.0`.
+
+            nonlin: Non-linearity. Defaults to `None`.
+
+            initializer: Weight initializer. Defaults to `tf.random_normal_initializer(stddev=0.1)`.
+
+            optimizer: Optimizer algorithm used for SGD. Defaults to `tf.train.AdamOptimizer(learning_rate=1e-2)`.
+
+            session: Tensorflow Session the model is run with. Defaults to `tf.Session()`.
+
+            name: Name of the End-To-End Memory Network. Defaults to `MemN2N`.
+        """
+
+        self._cands = cands
+        self.num_cands = len(cands)
+        self._batch_size = batch_size
+        self._vocab_size = vocab_size
+        self._sentence_size = sentence_size
+        self._memory_size = memory_size
+        self._embedding_size = embedding_size
+        self._hops = hops
+        self._max_grad_norm = max_grad_norm
+        self._nonlin = nonlin
+        self._init = initializer
+        self._name = name
+
+        self._build_inputs()
+        self._build_vars()
+
+        self._opt = optimizer
+
+        # cross entropy
+        logits = self._inference(self._stories, self._queries) # (batch_size, vocab_size)
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, 
+                                                                labels=tf.cast(self._answers, tf.float32), 
+                                                                name="cross_entropy")
+        # loss op
+        cross_entropy_sum = tf.reduce_sum(cross_entropy, name="cross_entropy_sum")
+
+        # gradient pipeline
+        grads_and_vars = self._opt.compute_gradients(cross_entropy_sum)
+        grads_and_vars = [(tf.clip_by_norm(g, self._max_grad_norm), v) for g,v in grads_and_vars]
+        nil_grads_and_vars = []
+        for g, v in grads_and_vars:
+            if v.name in self._nil_vars:
+                nil_grads_and_vars.append((zero_nil_slot(g), v))
+            else:
+                nil_grads_and_vars.append((g, v))
+        train_op = self._opt.apply_gradients(nil_grads_and_vars, name="train_op")
+
+        # predict ops
+        predict_op = tf.argmax(logits, 1, name="predict_op")
+        predict_proba_op = tf.nn.softmax(logits, name="predict_proba_op")
+        predict_log_proba_op = tf.log(predict_proba_op, name="predict_log_proba_op")
+
+        # assign ops
+        self.loss_op = cross_entropy_sum
+        self.predict_op = predict_op
+        self.predict_proba_op = predict_proba_op
+        self.predict_log_proba_op = predict_log_proba_op
+        self.train_op = train_op
+
+        init_op = tf.global_variables_initializer()
+        self._sess = session
+        self._sess.run(init_op)
+        self.saver = tf.train.Saver(max_to_keep=1)
+
+    def _build_inputs(self):
+        # self._stories = tf.placeholder(tf.int32, [None, self._memory_size, self._sentence_size], name="stories")
+        self._stories = tf.placeholder(tf.int32, [None, None, self._sentence_size], name="stories")
+        self._queries = tf.placeholder(tf.int32, [None, self._sentence_size], name="queries")
+        self._answers = tf.placeholder(tf.int32, [None, self.num_cands], name="answers")
+
+    def _build_vars(self):
+        with tf.variable_scope(self._name):
+            nil_word_slot = tf.zeros([1, self._embedding_size])
+            A = tf.concat(axis=0, values=[ nil_word_slot, self._init([self._vocab_size-1, self._embedding_size]) ])
+            W = tf.concat(axis=0, values=[ nil_word_slot, self._init([self._vocab_size-1, self._embedding_size]) ])
+  
+            self.LUT_A = tf.Variable(A, name="LUT_A")
+            self.LUT_W = tf.Variable(W, name="LUT_W")
+
+            # Dont use projection for layerwise weight sharing
+            self.R_proj = tf.Variable(self._init([self._embedding_size, self._embedding_size]), name="R_proj")
+
+        self._nil_vars = set([self.LUT_A.name, self.LUT_W.name])
+
+    def _inference(self, stories, queries):
+        with tf.variable_scope(self._name):
+            q_emb = tf.nn.embedding_lookup(self.LUT_A, queries)
+            u_0 = tf.reduce_sum(q_emb, 1)
+            u = [u_0]
+
+            for hopn in range(self._hops):
+                m_emb_A = tf.nn.embedding_lookup(self.LUT_A, stories)
+                m_A = tf.reduce_sum(m_emb_A, 2)
+
+                # hack to get around no reduce_dot
+                u_temp = tf.transpose(tf.expand_dims(u[-1], -1), [0, 2, 1])
+                dotted = tf.reduce_sum(m_A * u_temp, 2)
+
+                # Calculate probabilities
+                probs = tf.nn.softmax(dotted)
+
+                probs_temp = tf.transpose(tf.expand_dims(probs, -1), [0, 2, 1])
+
+                # Reuse A for the output memory encoding
+                c_temp = tf.transpose(m_A, [0, 2, 1])
+                o_k = tf.reduce_sum(c_temp * probs_temp, 2)
+
+                # Project hidden state, and add update
+                u_k = tf.matmul(u[-1], self.R_proj) + o_k
+
+                # nonlinearity
+                if self._nonlin:
+                    u_k = nonlin(u_k)
+
+                u.append(u_k)
+
+            cands_emb = tf.nn.embedding_lookup(self.LUT_W, self._cands)
+            cands_emb_sum = tf.reduce_sum(cands_emb, 1)
+
+            logits = tf.matmul(u_k, tf.transpose(cands_emb_sum))
+            
+            return logits
+
+    def batch_fit(self, stories, queries, answers):
+        """Runs the training algorithm over the passed batch
+
+        Args:
+            stories: Tensor (None, memory_size, sentence_size)
+            queries: Tensor (None, sentence_size)
+            answers: Tensor (None, vocab_size)
+
+        Returns:
+            loss: floating-point number, the loss computed for the batch
+        """
+        feed_dict = {self._stories: stories, self._queries: queries, self._answers: answers}
+        loss, _ = self._sess.run([self.loss_op, self.train_op], feed_dict=feed_dict)
+        
+        return loss
+
+    def predict(self, stories, queries):
+        """Predicts answers as one-hot encoding.
+
+        Args:
+            stories: Tensor (None, memory_size, sentence_size)
+            queries: Tensor (None, sentence_size)
+
+        Returns:
+            answers: Tensor (None, vocab_size)
+        """
+        feed_dict = {self._stories: stories, self._queries: queries}
+        return self._sess.run(self.predict_op, feed_dict=feed_dict)
