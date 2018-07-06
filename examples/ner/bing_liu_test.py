323d9da060065d4de45015ca47a30ea5c6675b3e
Łukasz Augustyniak <lukasz.augustyniak@spoken.com>
2018-07-06 14:31:48 +0200

    aspect extraction test

---
+++ examples/ner/bing_liu_test.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from train import run_ner
+
+if __name__ == '__main__':
+    datasets_path = Path('/home/lukasz/github/phd/sentiment-backend/aspects/data/aspects/bing_liu/bio_tags')
+    conll_train_files = list(datasets_path.glob('*train.conll'))
+    conll_test_files = list(datasets_path.glob('*test.conll'))
+
+    embedding_model = '/home/lukasz/data/glove.840B.300d.txt'
+
+    for train_file in conll_train_files:
+        test_file = [f for f in conll_test_files if train_file.stem.replace('train', '') in f.as_posix()][0]
+        run_ner(
+            train_file=train_file.as_posix(),
+            test_file=test_file.as_posix(),
+            embedding_model=embedding_model,
+            tag_num=2,
+            epoch=3,
+        )
