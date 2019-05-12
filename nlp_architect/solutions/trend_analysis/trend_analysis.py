f16a04a4b7f3f866679ba2afa706331d314ddc72
Shira Guskin <shira.guskin@intel.com>
2019-05-12 11:38:34 +0300

    style fix

--- nlp_architect/solutions/trend_analysis/trend_analysis.py
+++ nlp_architect/solutions/trend_analysis/trend_analysis.py
@@ -40,6 +40,7 @@ ref_topics_path = path.join(dir, 'ref_topics.csv')
 target_scores_path = path.join(dir, 'target_scores.csv')
 ref_scores_path = path.join(dir, 'ref_scores.csv')
 
+
 def analyze(target_data, ref_data, tar_header, ref_header, top_n=10000, top_n_vectors=500,
             re_analysis=False, tfidf_w=0.5, cval_w=0.5, lm_w=0):
     """
@@ -66,7 +67,7 @@ def analyze(target_data, ref_data, tar_header, ref_header, top_n=10000, top_n_ve
     create_clusters = False
     try:
         if not re_analysis:  # first analysis, not through ui
-            copyfile(target_data, target_topics_path) # copying the initial
+            copyfile(target_data, target_topics_path)  # copying the initial
             # scores file to have a const filename for the ui to recognize
             #  when reanalyzing
             copyfile(ref_data, ref_topics_path)
