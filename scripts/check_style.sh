830d0390c4b783d69e6bf0a2d76887779bb67c58
Peter Izsak <peter.izsak@intel.com>
2019-08-21 14:42:59 +0300

    update jenkins file for 0.5

--- scripts/check_style.sh
+++ scripts/check_style.sh
@@ -8,4 +8,8 @@ flake8 examples nlp_architect tests --config setup.cfg --format=html --htmldir=/
 echo "html output can be found in /tmp/nlp_architect_flake/index.html"
 echo "point your browser to that location to view status"
 echo ""
+echo "Running pylint check"
+pylint -j 4 examples tests nlp_architect --rcfile=pylintrc --score=n
+tee pylint.txt
+echo ""
 echo "Done."
