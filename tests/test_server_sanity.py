f59f4adcab648da62922f0f99af59a131c24dfc9
Peter Izsak <peter.izsak@intel.com>
2019-11-14 16:12:32 +0200

    minor

--- tests/test_server_sanity.py
+++ tests/test_server_sanity.py
@@ -114,8 +114,8 @@ def assert_response_struct(result_doc, expected_result):
             assert key in result_words
 
 
-@pytest.skip
 @pytest.mark.parametrize('service_name', ['bist', 'ner'])
+@pytest.skip
 def test_request(service_name):
     test_data = load_test_data(service_name)
     test_data['input']['model_name'] = service_name
@@ -150,8 +150,8 @@ def test_gzip_file_request(service_name):
     assert response.status == hug.HTTP_OK
 
 
-@pytest.skip
 @pytest.mark.parametrize('service_name', ['bist', 'ner'])
+@pytest.skip
 def test_json_file_request(service_name):
     file_path = os.path.join(os.path.dirname(__file__), server_data_rel_path + service_name
                              + "_sentences_examples.json")
