388d778b1cdfbf3541c4d31fa6e017ca495b069d
shira-g <shira.guskin@intel.com>
2019-11-28 15:48:14 +0200

    added RoBERTa head for question answering

--- nlp_architect/models/transformers/question_answering.py
+++ nlp_architect/models/transformers/question_answering.py
@@ -18,12 +18,17 @@ import os
 from typing import List, Union
 
 import torch
+from torch import nn
 from torch.utils.data import DataLoader, SequentialSampler, TensorDataset
+from torch.nn import CrossEntropyLoss
 from tqdm import tqdm, trange
 from transformers import (BertForQuestionAnswering,
                           XLMForQuestionAnswering,
-                          XLNetForQuestionAnswering)
-
+                          XLNetForQuestionAnswering,
+                          ROBERTA_PRETRAINED_MODEL_ARCHIVE_MAP,
+                          BertPreTrainedModel,
+                          RobertaConfig,
+                          RobertaModel)
 from nlp_architect.models.transformers.quantized_bert import QuantizedBertForQuestionAnswering
 from nlp_architect.utils.utils_squad import SquadExample
 from nlp_architect.models.transformers.base_model import TransformerBase
@@ -39,6 +44,58 @@ from nlp_architect.utils.utils_squad_evaluate import EVAL_OPTS, main as evaluati
 logger = logging.getLogger(__name__)
 
 
+class RobertaForQuestionAnswering(BertPreTrainedModel):
+    """RoBERTa question answering head.
+    """
+    config_class = RobertaConfig
+    pretrained_model_archive_map = ROBERTA_PRETRAINED_MODEL_ARCHIVE_MAP
+    base_model_prefix = "roberta"
+
+    def __init__(self, config):
+        super(RobertaForQuestionAnswering, self).__init__(config)
+        self.num_labels = config.num_labels
+
+        self.roberta = RobertaModel(config)
+        self.qa_outputs = nn.Linear(config.hidden_size, config.num_labels)
+
+        self.init_weights()
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None,
+                start_positions=None, end_positions=None):
+
+        outputs = self.roberta(input_ids,
+                            attention_mask=attention_mask,
+                            token_type_ids=token_type_ids,
+                            position_ids=position_ids,
+                            head_mask=head_mask)
+
+        sequence_output = outputs[0]
+
+        logits = self.qa_outputs(sequence_output)
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+
+        outputs = (start_logits, end_logits,) + outputs[2:]
+        if start_positions is not None and end_positions is not None:
+            # If we are on multi-GPU, split add a dimension
+            if len(start_positions.size()) > 1:
+                start_positions = start_positions.squeeze(-1)
+            if len(end_positions.size()) > 1:
+                end_positions = end_positions.squeeze(-1)
+            # sometimes the start/end positions are outside our model inputs, we ignore these terms
+            ignored_index = start_logits.size(1)
+            start_positions.clamp_(0, ignored_index)
+            end_positions.clamp_(0, ignored_index)
+
+            loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+            start_loss = loss_fct(start_logits, start_positions)
+            end_loss = loss_fct(end_logits, end_positions)
+            total_loss = (start_loss + end_loss) / 2
+            outputs = (total_loss,) + outputs
+
+        return outputs  # (loss), start_logits, end_logits, (hidden_states), (attentions)
+
 class TransformerQuestionAnswering(TransformerBase):
     """
     Transformer question answering model
@@ -53,6 +110,7 @@ class TransformerQuestionAnswering(TransformerBase):
         tokens for the question.
     """
     MODEL_CLASS = {
+        'roberta': RobertaForQuestionAnswering,
         'bert': BertForQuestionAnswering,
         'quant_bert': QuantizedBertForQuestionAnswering,
         'xlnet': XLNetForQuestionAnswering,
