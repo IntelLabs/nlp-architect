631036ab855ee6a6f192e9b70bcec030da14f3bc
Peter Izsak <peter.izsak@intel.com>
2020-01-08 15:56:43 +0200

    Update CI

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
 echo "Running NLP Architect tests"
+python -m spacy download en
 pytest ./ -rs -v -n 20 --dist=loadfile --cov=nlp_architect --junit-xml=pytest_unit.xml
 echo "Done."
