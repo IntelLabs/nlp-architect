a1e3abf9479a64bc6c9d669e428b039fb8d1db08
Peter Izsak <peter.izsak@intel.com>
2020-01-08 16:21:54 +0200

    minor

--- scripts/check_black.sh
+++ scripts/check_black.sh
@@ -1,4 +1,15 @@
 #! /usr/bin/env bash
+function check_rc {
+    RC="$?"
+    if [[ RC -gt $1 ]]
+    then
+        echo "RC = $RC > $1"
+        echo "quitting ..."
+        exit $?
+    fi
+}
+
 echo "Running black check"
 black --check --line-length 100 --target-version py36 examples nlp_architect solutions tests
+check_rc 0
 echo "Done running black"
