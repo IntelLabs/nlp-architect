ba8dd0feae65f159b50f740208434f29b112177b
Peter Izsak <peter.izsak@intel.com>
2020-01-09 12:15:54 +0200

    Update crf.py

--- nlp_architect/nn/torch/layers/crf.py
+++ nlp_architect/nn/torch/layers/crf.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ******************************************************************************
+# Module adapted from https://github.com/kmkurn/pytorch-crf
 from typing import List, Optional
 
 import torch
