1b14d1d99fbbc518234d25f9a7d8db3a8879e6e4
Peter Izsak <peter.izsak@intel.com>
2020-01-09 10:47:04 +0200

    .

--- scripts/check_flake.sh
+++ scripts/check_flake.sh
@@ -11,11 +11,11 @@ function check_rc {
 }
 
 echo "Running flake8 check"
-flake8 examples nlp_architect tests --config setup.cfg --output-file /tmp/nlp_architect_flake_out.txt
+flake8 examples nlp_architect tests solutions --config setup.cfg --output-file /tmp/nlp_architect_flake_out.txt
 check_rc 0
 mkdir -p /tmp/nlp_architect_flake
 echo "exporting to html"
-flake8 examples nlp_architect tests --config setup.cfg --format=html --htmldir=/tmp/nlp_architect_flake
+flake8 examples nlp_architect tests solutions --config setup.cfg --format=html --htmldir=/tmp/nlp_architect_flake
 check_rc 0
 echo "html output can be found in /tmp/nlp_architect_flake/index.html"
 echo "point your browser to that location to view status"
